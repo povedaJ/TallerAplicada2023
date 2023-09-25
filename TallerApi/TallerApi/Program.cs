c6a84e6315b64b30a6244ad278e768741710fd1e
povedaJ <66696920+povedaJ@users.noreply.github.com>
2023-09-25 01:43:34 -0600

    Taller aplicada completo.

--- TallerApi/TallerApi/Program.cs
+++ TallerApi/TallerApi/Program.cs
@@ -19,5 +19,51 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.MapGet("/Customer", async (HotelParaisoContext context) =>
+{
+    return Results.Ok(await context.Customers.ToListAsync());
+});
+
+app.MapGet("/Customer{id}", async (Guid id, HotelParaisoContext context) =>
+{
+    if (await context.Customers.AnyAsync<Customer>(x => x.CustomerId == id))
+    {
+        return Results.Ok(await context.Customers.FirstAsync<Customer>(x => x.CustomerId == id));
+    }
+    return Results.NotFound();
+});
+
+app.MapPost("/Customer", async ([FromBody] Customer customer, HotelParaisoContext context) =>
+{
+    customer.CustomerId = Guid.NewGuid();
+    await context.Customers.AddAsync(customer);
+    await context.SaveChangesAsync();
+    return Results.Created($"/Customer/{customer.CustomerId}", customer);
+
+});
+
+app.MapPut("/Customer{id}", async (Guid id, [FromBody] Customer customer, HotelParaisoContext context) =>
+{
+    if (await context.Customers.AnyAsync<Customer>(x => x.CustomerId == id))
+    {
+        context.Entry(customer).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+        return Results.NoContent();
+    }
+    return Results.NotFound();
+});
+
+app.MapDelete("/Customer{id}", async (Guid id, HotelParaisoContext context) =>
+{
+    if (await context.Customers.FindAsync(id) is Customer customer)
+    {
+        context.Customers.Remove(customer);
+        await context.SaveChangesAsync();
+        return Results.NoContent();
+
+    }
+    return Results.NotFound();
+
+});
 
 app.Run();
